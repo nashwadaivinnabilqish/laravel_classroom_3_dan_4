32391677f95cca884a5f50f24f4e2dbf1c7cf4c0
Thinkpad <nashwadaivinnabilqish@gmail.com>
2026-04-02 11:23:19 +0700

    ini tugas 4

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SekolahController;
 // Route menggunakan Controller
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
 // use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
